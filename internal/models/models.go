5c0c5d71ba0975cd8fa3480f81a6cd25ee321401
techarm <techarmdev@gmail.com>
2022-05-12 22:22:40 +0900

    Add models

--- internal/models/models.go
+++ internal/models/models.go
@@ -39,3 +39,42 @@ type Preference struct {
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
+
+// Hosts Model
+type Hosts struct {
+	ID            int
+	HostName      string
+	CanonicalHost string
+	URL           string
+	IP            string
+	IPv6          string
+	Location      string
+	OS            string
+	Active        int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+// Services Model
+type Services struct {
+	ID          int
+	ServiceName string
+	Active      int
+	Icon        string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Hostservices Model
+type HostServices struct {
+	ID             int
+	HostID         int
+	ServiceID      int
+	Active         int
+	ScheduleNumber int
+	ScheduleUnit   string
+	Status         string
+	LastCheck      time.Time
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
